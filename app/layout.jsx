e9f243a353683aa864ba53c8bed0fda8305ded08
josh520n-cmd <josh520n@gmail.com>
2026-05-26 21:24:10 -0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0 }}>{children}</body>
+      <body style={{ margin: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
